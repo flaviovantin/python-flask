f429e2ca0b061d030133815b39c04e0746bfbd80
Flavio Vantin <flavio.vantin@gmail.com>
2023-03-02 22:44:07 -0500

    Adding templates

--- First App/first-app.py
+++ First App/first-app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('users.html')
+
+
+@app.route('/info')
+def information():
+    return '<h1>Information about the user machine...</h1>'
+
+
+@app.route('/users/<name>')
+def user(name):
+    # return f'<p>This is the user {name}</p>'
+    return '<h2>This is the user {}'.format(name[10])
 
 
 if __name__ == '__main__':
+    # app.run(debug=True)
     app.run()
