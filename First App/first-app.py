3655e3838473486d21315f7ec798390df8917eb4
Flavio Vantin <flavio.vantin@gmail.com>
2023-03-02 23:17:40 -0500

    Added Jinja template

--- First App/first-app.py
+++ First App/first-app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('users.html')
+    return render_template('welcome.html')
 
 
 @app.route('/info')
@@ -13,9 +13,17 @@ def information():
     return '<h1>Information about the user machine...</h1>'
 
 
-@app.route('/users/<name>')
-def user(name):
-    return '<h2>This is the user {}'.format(name)
+@app.route('/users/<user_id>')
+def user(user_id):
+    users = [
+        {
+            'id': user_id,
+            'first_name': 'Lord',
+            'last_name': 'Tobias I',
+            'age': 15
+        }
+    ]
+    return render_template('users.html', users=users)
 
 
 if __name__ == '__main__':
