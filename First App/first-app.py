6ccf7d6baf3941eabca953f77d35fa062eae4314
Flavio Vantin <flavio.vantin@gmail.com>
2023-03-08 22:07:38 -0500

    Added advanced templates

--- First App/first-app.py
+++ First App/first-app.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('welcome.html')
+    return render_template('home.html')
 
 
-@app.route('/info')
-def information():
-    return '<h1>Information about the user machine...</h1>'
+@app.route('/beers')
+def beers():
+    return render_template('beers.html')
 
 
 @app.route('/users/<user_id>')
-def user(user_id):
+def users(user_id):
     users = [
         {
             'id': user_id,
@@ -24,6 +24,11 @@ def user(user_id):
         }
     ]
     return render_template('users.html', users=users)
+
+
+@app.route('/about')
+def about():
+    return '<h1>Information about the user machine...</h1>'
 
 
 if __name__ == '__main__':
